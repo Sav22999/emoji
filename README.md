d48247d572ed4edf759f1b29c24e3373cab6d62d
Saverio <saverio.morelli@outlook.it>
2020-01-29 23:21:51 +0100

    Updated to 0.2
    - Improved code
    - Added sections  (solved #1 )
    - Changed font: "Source Code Pro" -> "Twemoji"
    - Added hundreds new emojis

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
-# Emoji
-This extension permits you to copy an emoji in the clipboard.
-It is published on Firefox Addons https://addons.mozilla.org/it/firefox/addon/emoji-sav/.
+# <img src="img/icon-dark-96.png" width="40px" alt=""></img> Emoji
+Official repo of https://addons.mozilla.org/it/firefox/addon/emoji-sav/.
+
+[![GitHub release](https://img.shields.io/github/release/Sav22999/emoji.svg)](https://github.com/Sav22999/emoji/releases/) [![GitHub license](https://img.shields.io/github/license/Sav22999/emoji.svg)](https://github.com/Sav22999/emoji/blob/master/LICENSE) ![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)
+
+[<img src="img/badge.png" height="60px">](https://addons.mozilla.org/it/firefox/addon/emoji-sav/) [<img src="img/a-project-of.png" height="60px" />](https://saveriomorelli.com) [<img src="img/realised-by.png" height="60px" />](https://saveriomorelli.com)
+
+## Description
+
+This extension permits you to copy an emoji in the clipboard. There are many emojis, exactly 905!
+
+Emojis are divided in some sections, to help you to find the correct emoji quickly (in order):
+
+- Smileys (132 emojis)
+- Animals (111 emojis)
+- Symbols (162 emojis)
+- Foods and drinks (105 emojis)
+- Flags (62 emojis)
+- Sports and everything related to them (53 emojis)
+- Technologies and office (68 emojis)
+- Clothes and accessories (34 emojis)
+- Hands (41 emojis)
+- Other (“not categorised”) (137 emojis)
 
 ## How to contribute
+
 You can open an issue and there you must describe the feedback, the bug or the new feature you want.
 
 ## Screenshots
 
-Add-ons UI:
+New UI:
 
-<img src="screenshots/screenshot.png" alt="screenshot old" style="zoom:50%;" />
+<img src="screenshots/screenshot.png" alt="screenshot"  height="500px" />
+
+(old) UI:
+
+<img src="screenshots/old_screenshot.png" alt="screenshot old"  height="400px" />
--- js/script.js
+++ js/script.js
@@ -1,31 +1,90 @@
-var emojis = ["😀", "😄", "😁", "😆", "😅", "😂", "🙂", "😉", "😇", "😍", "🤩", "😘", "😗", "😛", "😜", "🤗", "🤢", "🤮", "🥵", "🥶", "☹️", "😮", "😨", "😰", "😭", "😱", "😤", "😡", "😈", "👌", "✌️", "👍", "👎", "🙏"];
-var n_emojis = 0;
+var titles = ["😀", "🦊", "🅰️", "🍎", "🏳️‍🌈", "🏊", "🖱️", "👗", "🖐️", "🛎️"];
+var emojis = [0, 1, 2];
+/*smileys*/
+emojis[0] = ["😀", "😃", "😄", "😁", "😆", "😅", "😅", "🤣", "😂", "🙂", "🙃", "😉", "😊", "😇", "🥰", "😍", "🤩", "😘", "😗", "☺️", "😚", "😙", "😋", "😛", "😜", "🤪", "😝", "🤑", "🤗", "🤭", "🤫", "🤔", "🤐", "🤨", "😐", "😑", "😶", "😏", "😒", "🙄", "😬", "🤥", "😌", "😔", "😪", "🤤", "😴", "😷", "🤒", "🤕", "🤢", "🤮", "🤧", "🥵", "🥶", "🥴", "😵", "🤯", "🤠", "🥳", "😎", "🤓", "🧐", "😕", "😟", "🙁", "☹️", "😮", "😯", "😲", "😳", "🥺", "😦", "😨", "😰", "😥", "😢", "😭", "😱", "😖", "😣", "😞", "😓", "😩", "😫", "😤", "😡", "😠", "🤬", "😈", "👿", "🤡", "👽", "👾", "🤖", "👹", "👻", "👶", "🧒", "👧", "🧑", "👱", "👨", "🧔", "👨‍🦰", "👨‍🦱", "👨‍🦳", "👨‍🦲", "👩", "👩‍🦰", "👩‍🦱", "👩‍🦳", "👩‍🦲", "👱‍♀️", "👱‍♂️", "🧓", "👴", "👵", "👼", "🎅", "🤶", "🤴", "👸", "👳", "👳‍♂️", "👳‍♀️", "👲", "🧕", "🤵", "👰", "🤰", "🤱"];
+/*animals*/
+emojis[1] = ["😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾", "🙈", "🙉", "🙊", "🐵", "🐒", "🦍", "🐶", "🐕", "🐩", "🐺", "🦊", "🦝", "🐱", "🐈", "🦁", "🐯", "🐅", "🐆", "🐴", "🐎", "🦄", "🦓", "🦌", "🐮", "🐂", "🐃", "🐄", "🐷", "🐖", "🐗", "🐽", "🐏", "🐑", "🐐", "🐪", "🐫", "🦙", "🦒", "🐘", "🦏", "🦛", "🐭", "🐁", "🐀", "🐹", "🐰", "🐇", "🐿️", "🦔", "🦇", "🐻", "🐨", "🐼", "🦘", "🦡", "🦃", "🐔", "🐓", "🐣", "🐤", "🐥", "🐦", "🐧", "🕊️", "🦅", "🦆", "🦢", "🦉", "🦚", "🦜", "🐸", "🐊", "🐢", "🦎", "🐍", "🐲", "🐉", "🦕", "🦖", "🐳", "🐋", "🐬", "🐟", "🐠", "🐡", "🦈", "🐙", "🐚", "🐌", "🦋", "🐛", "🐜", "🐝", "🐞", "🦗", "🕷️", "🦂", "🦟", "🦀", "🦞", "🦐", "🦑"];
+/*symbols*/
+emojis[2] = ["💘", "💝", "💖", "💗", "💓", "💞", "💕", "💟", "❣️", "💔", "❤️", "🧡", "💛", "💚", "💙", "💜", "🖤", "🤍", "🤎", "💯", "💬", "💤", "♠️", "♥️", "♦️", "♣️", "🏧", "🎵", "🎶", "🔔", "🔕", "🔈", "🔉", "🔊", "🔇", "⛔", "🚫", "🚳", "🚭", "🚯", "🚱", "🚷", "🔞", "☢️", "☣️", "⬆️", "↗️", "➡️", "↘️", "⬇️", "↙️", "⬅️", "↖️", "↕️", "↔️", "↩️", "↪️", "⤴️", "⤵️", "🔃", "🔄", "🔙", "🔚", "🔛", "🔜", "🔝", "🛐", "⚛️", "🕉️", "✡️", "☸️", "☯️", "✝️", "☦️", "☪️", "☮️", "🕎", "🔯", "♈", "♉", "♊", "♋", "♌", "♍", "♎", "♏", "♐", "♑", "♒", "♓", "⛎", "🔀", "🔁", "🔂", "▶️", "◀️", "🔼", "🔽", "⏩", "⏪", "⏫", "⏬", "⏹️", "⏏️", "🎦", "📶", "✖️", "➕", "➖", "➗", "♾️", "‼️", "⁉️", "❓", "❔", "❕", "❗", "⭕", "☑️", "✔️", "❌", "✅", "❎", "#️⃣", "0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟", "🔠", "🔡", "🔢", "🔤", "🅰️", "🆎", "🅱️", "🆑", "🆒", "🆓", "ℹ️", "🆔", "🆕", "🅾️", "🆗", "🅿️", "🆘", "🆙", "🆚", "🔴", "🔵", "⚫", "⚪", "⬛", "⬜", "🔶", "🔷", "🔸", "🔹", "🔺", "🔻", "🔳", "🔲", "©️", "®️", "™️"];
+/*food and drinks*/
+emojis[3] = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🥝", "🍅", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🥒", "🥬", "🥦", "🍄", "🥜", "🌰", "🍞", "🥐", "🥖", "🥨", "🥯", "🥞", "🧀", "🍖", "🍗", "🥩", "🥓", "🍔", "🍟", "🍕", "🌭", "🥪", "🌮", "🌯", "🥙", "🍳", "🥘", "🍲", "🥣", "🥗", "🍿", "🧂", "🥫", "🍱", "🍘", "🍙", "🍚", "🍛", "🍜", "🍝", "🍠", "🍢", "🍣", "🍤", "🍥", "🥮", "🍡", "🥟", "🥠", "🥡", "🍦", "🍧", "🍨", "🍩", "🍪", "🎂", "🍰", "🧁", "🥧", "🍫", "🍬", "🍭", "🍮", "🍯", "🍼", "🥛", "☕", "🍵", "🍶", "🍾", "🍷", "🍸", "🍹", "🍺", "🍻", "🥂", "🥃", "🥤", "🥢", "🍽️", "🍴", "🥄"];
+/*flags*/
+emojis[4] = ["🏁", "🚩", "🎌", "🏴", "🏳️", "🏳️‍🌈", "🏳️‍⚧️", "🏴‍☠️", "🇺🇳", "🇪🇺", "🇧🇸", "🇨🇦", "🇨🇭", "🇨🇳", "🇩🇪", "🇩🇰", "🇪🇪", "🇪🇬", "🇪🇸", "🇫🇮", "🇫🇷", "🇬🇧", "🇬🇱", "🇬🇷", "🇭🇺", "🇮🇪", "🇮🇱", "🇮🇩", "🇮🇳", "🇮🇶", "🇮🇷", "🇮🇸", "🇮🇹", "🇯🇵", "🇯🇲", "🇰🇪", "🇰🇷", "🇰🇵", "🇱🇺", "🇲🇹", "🇲🇽", "🇳🇴", "🇳🇱", "🇳🇿", "🇵🇱", "🇵🇹", "🇷🇺", "🇸🇪", "🇸🇮", "🇹🇳", "🇺🇦", "🇺🇸", "🇻🇪", "🇻🇬", "🇻🇮", "🇿🇦", "🇿🇼", "🇦🇪", "🇦🇷", "🇦🇹", "🇦🇺", "🇧🇷"];
+/*sports*/
+emojis[5] = ["🧗", "🏇", "⛷️", "🏂", "🏌️", "🏄", "🚣", "🏊", "⛹️", "🏋️", "🚴", "🚵", "🤸", "🤼", "🤽", "🤾", "🤹", "🧘", "🏆", "🏅", "🥇", "🥈", "🥉", "⚽", "⚾", "🥎", "🏀", "🏐", "🏈", "🏉", "🎾", "🥏", "🎳", "🏏", "🏑", "🏒", "🥍", "🏓", "🏸", "🥊", "🥋", "⛳", "⛸️", "🎣", "🎽", "🎿", "🛷", "🥌", "🎯", "🎱", "🎮", "♟️", "🏹"];
+/*technologies (smartphones, laptops, ect.) and office*/
+emojis[6] = ["⌚", "⏰", "⏱️", "⏲️", "🕰️", "🕹️", "🎙️", "🎚️", "🎛️", "📻", "📱", "📲", "☎️", "📞", "📟", "📠", "🔋", "🔌", "💻", "🖥️", "🖨️", "⌨️", "🖱️", "🖲️", "💽", "💾", "💿", "📀", "🎥", "🎞️", "📽️", "📺", "📷", "📸", "📹", "📼", "💳", "📧", "📨", "📩", "📤", "📥", "📮", "📁", "📂", "🗂️", "📅", "📈", "📉", "📊", "📌", "📋", "📍", "📎", "📏", "📐", "✂️", "✏️", "✒️", "🖋️", "🖊️", "🖌️", "🖍️", "📝", "🗑️", "📡", "🔭", "🔬"];
+/*clothes and accessories*/
+emojis[7] = ["👣", "👓", "🕶️", "🥽", "🥼", "👔", "👕", "👖", "🧣", "🧤", "🧥", "🧦", "👗", "👘", "👙", "👚", "👛", "👜", "👝", "🎒", "👞", "👟", "🥾", "🥿", "👠", "👡", "👢", "👑", "👒", "🎩", "🎓", "🧢", "⛑️", "💼"]
+/*hands and parts of body*/
+emojis[8] = ["👋", "🤚", "✋", "🖖", "👌", "✌️", "🤞", "🤟", "🤘", "🤙", "🤏", "👈", "👉", "👆", "👇", "☝️", "🖕", "👍", "👎", "✊", "👊", "🤛", "🤜", "👏", "🙌", "👐", "🤲", "🤝", "🙏", "✍️", "💪", "🦵", "🦶", "👂", "👃", "🧠", "🦷", "👀", "👁️", "👅", "👄"]
+/*other*/
+emojis[9] = ["💩", "💋", "💅", "🤳", "🦴", "🧳", "🌂", "☂️", "🧵", "🧶", "💄", "💍", "💥", "💫", "💦", "💨", "🐾", "🕸️", "🦠", "💐", "🌍", "🌎", "🌏", "🌐", "🌑", "🌓", "🌕", "🌗", "🌙", "🌛", "🌜", "☀️", "⭐", "☁️", "⛅", "⛈️", "🌤️", "🌥️", "🌦️", "🌧️", "🌨️", "🌩️", "🌪️", "🌫️", "🌈", "❄️", "☃️", "⛄", "☄️", "🔥", "💧", "🌊", "🎄", "🌻", "🌼", "🌷", "🌹", "🌹", "🌳", "🌴", "☘️", "🔑", "🗝️", "🔨", "⛏️", "🔒", "🔓", "🔏", "🔐", "🗡️", "⚔️", "🔫", "🧰", "🧲", "⛓️", "💉", "🧬", "🧪", "🛏️", "🚪", "🚽", "🚿", "🛁", "🧴", "🧷", "🧺", "🧻", "🧼", "🧽", "🧯", "🚬", "⚰️", "⚱️", "🗿", "🛡️", "⚙️", "⚖️", "🔗", "💀", "☠️", "🦻", "🦸", "🦸‍♀️", "🦹", "🦹‍♀️", "🧙", "🧙‍♀️", "🧚", "🧚‍♀️", "🧛", "🧛‍♀️", "🧜", "🧜‍♀️", "🧝", "🧝‍♀️", "🧞‍♂️", "🧞", "🕵️", "💂", "💂‍♀️", "👮", "👮‍♀️", "👨‍🚒", "👩‍🚒", "👨‍‍✈️", "👩‍✈️", "👨‍‍💻", "👩‍💻", "👨‍‍🎤", "👩‍🎤", "👨‍‍🎨", "👩‍🎨", "👷‍♂️", "👷‍♀️", "💑", "👨‍❤️‍👨", "👩‍❤️‍👩"];
+var selectedTitle = 0;
 
-function copyEmoji(text)
-{
-	document.getElementById("text_to_copy").style.display="block";
-	document.getElementById("text_to_copy").value = text;
-	var copyText=document.getElementById("text_to_copy");
-	copyText.select();
-	document.execCommand("copy");
-	document.getElementById("text_to_copy").style.display="none";
+generateTitles();
+
+function copyEmoji(text) {
+    document.getElementById("text_to_copy").style.display = "block";
+    document.getElementById("text_to_copy").value = text;
+    var copyText = document.getElementById("text_to_copy");
+    copyText.select();
+    document.execCommand("copy");
+    document.getElementById("text_to_copy").style.display = "none";
 }
 
-function setEmojis()
-{
-
-	//var n_temp = document.getElementsByClassName("symbol").length;
-	//n_symbols = n_temp;
-	n_emojis = emojis.length;
-	for(var i=0;i<n_emojis;i++)
-	{
-		document.getElementById("emojis").innerHTML += "<input type=\"button\" class=\"emoji\" value=\""+emojis[i]+"\" />";
-	}
+function generateTitles() {
+    var widthToSet = 100 / titles.length;
+    for (var i = 0; i < titles.length; i++) {
+        document.getElementById("titles").innerHTML += "<input type='button' class='section_title' id='title" + i + "' value='" + titles[i] + "' />";
+        document.getElementsByClassName("section_title")[i].style.width = widthToSet + "%";
+    }
+    for (var i = 0; i < titles.length; i++) {
+        document.getElementsByClassName("section_title")[i].onclick = function (e) {
+            setTitle(this.id.replace("title", ""));
+        };
+    }
+    setTitle(0);
 }
 
-setEmojis();
+function setTitle(newTitle) {
+    document.getElementsByClassName("section_title")[selectedTitle].style.borderTopColor = "transparent";
+    selectedTitle = newTitle;
+    document.getElementsByClassName("section_title")[selectedTitle].style.borderTopColor = "rgb(10, 132, 255)";
+    generateEmojis(newTitle);
+}
 
-for(var i=0;i<n_emojis;i++)
-{
-	document.getElementsByClassName("emoji")[i].onclick = function(e){copyEmoji(this.value);};
+function generateEmojis(title) {
+    //emoji[title][i]
+    document.getElementById("emojis").innerHTML = "";
+    var n_emojis = emojis[title].length;
+    for (var i = 0; i < n_emojis; i++) {
+        document.getElementById("emojis").innerHTML += "<input type=\"button\" class=\"emoji\" value=\"" + emojis[title][i] + "\" />";
+    }
+
+    for (var i = 0; i < n_emojis; i++) {
+        document.getElementsByClassName("emoji")[i].onclick = function (e) {
+            copyEmoji(this.value);
+        };
+    }
+    setHeight();
+}
+
+function setHeight() {
+    var max_columns = 9;
+    var max_rows = 6;
+    var n_emojis = emojis[selectedTitle].length;
+    var rows = parseInt(n_emojis / max_columns + "");
+    if ((n_emojis % max_columns) != 0) rows += 1;
+    document.getElementById("emojis").style.height = max_rows * 50 + 4 + "px";
+    document.getElementById("popup-content").style.height = max_rows * 50 + 4 + 36 + "px"; //36 is the height of titles
+
+    var widthToSet = max_columns * 50 + 4 + 10; //50 is the height of one row, 4 is the padding of emojis div, 10 is the width of scrollbar (customised), otherwise it would be 18
+    if (rows > max_rows) {
+        document.body.style.width = widthToSet + "px";
+    } else {
+        document.body.style.width = max_columns * 50 + 4 + "px";
+    }
+    //var n_emojies_in_section = rows * max_columns - (max_columns - n_emojis % max_columns);
+    document.getElementById("emojis").scrollTop = (0, 0);
 }
