d83cac7beaf30a7d3bab8de34b2e35ade4979f90
Sav22999 <saverio.morelli@outlook.it>
2021-01-18 00:51:09 +0100

    3.9

--- js/release_notes.js
+++ js/release_notes.js
@@ -4,7 +4,7 @@ function releaseNotes(release) {
         case "3.9":
             release_notes_text = "<ul>";
             release_notes_text += "<li>You can support this project buying a coffee to the developer on PayPal or Ko-fi (see the Settings bottom) {{emoji}}☕{{/emoji}}</li>";
-            release_notes_text += "<li>Added the shortcut to open this add-on: Ctrl+Alt+A</li>";
+            release_notes_text += "<li>Added the shortcut to open this add-on: Ctrl/Command+Alt+A (only in Firefox!)</li>";
             release_notes_text += "<li>Press \"Enter\" when finish to digit the keyword to focus automatically the results</li>";
             release_notes_text += "</ul>";
             break;
