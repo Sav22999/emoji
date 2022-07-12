9b0bf85ce6af0e0bdb1f41e2b4863420f1429789
Saverio Morelli <saverio.morelli@outlook.it>
2022-07-12 11:56:44 +0200

    edge

--- js/emoji-insert-directly.js
+++ js/emoji-insert-directly.js
@@ -1,5 +1,5 @@
 var browserAgentSettings = "";
-var browserOrChromeIndex = 0; //TODO: change manually: {0: Firefox, 1: Microsoft Edge, 2: Chrome Web Store}
+var browserOrChromeIndex = 1; //TODO: change manually: {0: Firefox, 1: Microsoft Edge, 2: Chrome Web Store}
 if (browserOrChromeIndex === 0) {
     browserAgentSettings = browser;
 } else if (browserOrChromeIndex === 1 || browserOrChromeIndex === 2) {
