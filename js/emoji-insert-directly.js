6435109ff918bd11bbad527f22f51050693e3a94
Saverio Morelli <saverio.morelli@outlook.it>
2023-03-24 09:55:18 +0100

    3.17
    - Fully implemented "Insert directly" also for elements which supports "contenteditable" -- Fixed some bugs

--- js/emoji-insert-directly.js
+++ js/emoji-insert-directly.js
@@ -13,17 +13,27 @@ browserAgentSettings.runtime.onMessage.addListener(function (request, sender, se
             let pre = (active.value).substring(0, active.selectionStart);
             let post = (active.value).substring(active.selectionEnd, active.value.length);
             active.value = pre + request.emoji + post;
-        } else if (["p", "div", "span"].includes(active.tagName.toLowerCase()) || active.contentEditable) {
-            let position = {start: 0, end: 0};
-            let selection = document.getSelection();
-            let range = selection.getRangeAt(0);
-            let range2 = range.cloneRange();
-            range2.selectNodeContents(active);
-            position.start = range.startOffset;
-            position.end = range.endOffset;
-            let pre = (active.textContent).substring(0, position.start);
-            let post = (active.textContent).substring(position.end, active.textContent.length);
-            active.textContent = pre + request.emoji + post;
+        } else if (["p", "div", "span"].includes(active.tagName.toLowerCase()) && active.contentEditable) {
+            try {
+                let position = {start: 0, end: 0};
+                let selection = document.getSelection();
+                let range = selection.getRangeAt(0);
+                let range2 = range.cloneRange();
+                range2.selectNodeContents(active);
+                position.start = range.startOffset;
+                position.end = range.endOffset;
+                let pre = (active.textContent).substring(0, position.start);
+                let post = (active.textContent).substring(position.end, active.textContent.length);
+                active.textContent = pre + request.emoji + post;
+
+                let range3 = document.createRange();
+                range3.setStart(active.childNodes[0], position.start + (request.emoji.length));
+                range3.setEnd(active.childNodes[0], position.start + (request.emoji.length));
+                selection.removeAllRanges();
+                selection.addRange(range3);
+            } catch (e) {
+                console.error("Exception: " + e);
+            }
         }
         lastRequestNumber = request.requestNumber; //update the request number used to avoid to show more emoji (because of more injection scripts)
     }
